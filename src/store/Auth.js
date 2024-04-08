4ebc08aa36cda0b66cc9ca5013b3e878f2f40508
sumeetmishra1 <sumitsfs0@gmail.com>
2024-04-08 14:21:08 +0530

    state manangement

---
+++ src/store/Auth.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState ={
+    isAuthenticated: false,
+    token:null,
+    email:null
+}
+
+const auth = createSlice({
+    name:"auth",
+    initialState,
+    reducers:{
+        login:(state,action)=>{
+            console.log(action.payload)
+               state.email = action.payload.emailPart
+                state.isAuthenticated= true;
+                state.token = action.payload.token;
+                console.log(JSON.stringify(state))
+        
+        },
+        logout:(state,action)=>{
+           state.email = null;
+            state.token = null;
+            state.isAuthenticated=false;
+            
+        }
+    }
+})
+
+export const {login,logout} = auth.actions;
+export default auth.reducer;
