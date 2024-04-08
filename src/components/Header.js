253b6defefb7133d0f74aaa0c0a2a676a0d0f631
sumeetmishra1 <sumitsfs0@gmail.com>
2024-04-08 14:13:28 +0530

    header

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { NavLink, useNavigate } from 'react-router-dom';
+import {logout} from "../store/Auth"
+
+const Header = () => {
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch();
+
+  const handleLogout =()=>{
+    dispatch(logout())
+    /// navigate issue hai
+   window.location.replace('/')
+  }
+  return (
+    <div className='flex justify-between bg-slate-400 px-3 text-white font-semibold'>
+      <NavLink to="/login">Login</NavLink>
+      <NavLink to="/">Signup</NavLink>
+      <NavLink to="/dashboard">Dashboard</NavLink>
+      <NavLink to="/sentMail">Outbox</NavLink>
+      <NavLink onClick={handleLogout} >logout</NavLink>
+    </div>
+  );
+};
+
+export default Header;
